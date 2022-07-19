4c4f4de54e7a2c4469c58fcf4e12d00de1ca47cb
DmitryVenchikov <78564900+DmitryVenchikov@users.noreply.github.com>
2022-07-19 14:57:31 +0300

    done

---
+++ Angular/Angular/ClientApp/src/app/get-particular/get-particular.component.ts
@@ -0,0 +1,50 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-get-particular',
+  templateUrl: './get-particular.component.html'
+})
+
+export class GetParticularComponent {
+  public forecast: WeatherForecast;
+  public forecasts: WeatherForecast[] = [];
+  public errorString: string;
+  public id: number;
+  public http: HttpClient;
+  public url: string;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+
+    this.errorString = "Номер еще не введен";
+    this.http = http;
+    this.url = baseUrl;
+  }
+
+  getItem(): void {
+    if (this.id >= this.forecasts.length || 0 > this.id) {
+      this.errorString = "Строки с таким индексом не существует";
+      this.forecast = undefined;
+    }
+    else if (typeof (this.id) != "number") {
+      this.errorString = "Индекс должен быть числом";
+      this.forecast = undefined;
+    }
+    else {
+      this.http.get<WeatherForecast>(this.url + 'getparticular?id=' + this.id).subscribe(result => {
+        this.forecast = result;
+      }, error => console.error(error));
+    }
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
